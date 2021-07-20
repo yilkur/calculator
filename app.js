749062af2a3bf98df176db4493c8953182338a3b
Yilmaz Kurnazcan <ykurnazcan@tocco.ch>
2021-07-20 13:27:14 +0200

    add keyboard support

--- app.js
+++ app.js
@@ -9,80 +9,57 @@ const divide = nums =>
   nums.includes(0) ? 'NO ZERO DIVISION' : nums.reduce((acc, val) => acc / val)
 
 const operate = (operationType, nums) => {
-  operationType = operationType.toUpperCase()
-
-  if (operationType === '+' || operationType === 'Add') {
-    return add(nums)
-  }
-  if (operationType === '-' || operationType === 'Subtract') {
-    return subtract(nums)
-  }
-  if (operationType === '*' || operationType === 'Multiply') {
-    return multiply(nums)
-  }
-  if (operationType === '/' || operationType === 'Divide') {
-    return divide(nums)
-  }
+  if (operationType === '+') return add(nums)
+  if (operationType === '-') return subtract(nums)
+  if (operationType === '*') return multiply(nums)
+  if (operationType === '/') return divide(nums)
 }
 
+screen.innerText = '0'
+let nums = []
+let num = ''
+let operationType
+let operatorPressed = false
+
 const clearAll = () => {
-  screen.innerText = ''
+  screen.innerText = '0'
   nums = []
   num = ''
 }
 
 const allAbuttonsArr = [...allButtons]
 
-let nums = []
-let num = ''
-let operationType
-
-const getKeyBoardInput = e => {
-  const key = e.code
-
-  if (key && key !== 'NumLock') {
-    const isAllowedKey = key => key.includes('Numpad') || key.includes('Digit')
-    let number
-    let operation
-
-    if (isAllowedKey(key)) {
-      const isNum = key.match(/[0-9]/g)
-
-      if (Boolean(isNum)) {
-        number = Number(isNum[0])
-      } else {
-        operation = key.split('Numpad')[1]
-      }
-    }
-
-    const output = typeof number === 'number' ? number : { operator: operation }
-    console.log(output)
-    return output
+const processInput = input => {
+  if (screen.innerText === '0') {
+    screen.innerText = ''
   }
-}
 
-const processInput = e => {
-  const buttonType = e.target.getAttribute('data-type')
-  const buttonContent = e.target.innerText
   let result
 
-  if (buttonType === 'clear') {
+  if (input === 'AC') {
     clearAll()
-  } else if (buttonType === 'dot') {
+  } else if (input === '.') {
     if (!screen.innerText.includes('.')) {
       screen.innerText += '.'
     }
-  } else if (buttonType === 'operator') {
-    operationType = buttonContent
+  } else if (input === 'DEL') {
+    if (screen.innerText.length > 1) {
+      screen.innerText = screen.innerText.slice(0, screen.innerText.length - 1)
+    } else {
+      screen.innerText = 0
+    }
+  } else if (input === '+' || input === '-' || input === '*' || input === '/') {
+    operatorPressed = true
+    operationType = input
     nums.push(Number(screen.innerText))
     if (nums.length === 2) {
       result = operate(operationType, nums)
       nums = [result]
       screen.innerText = result
     } else {
-      screen.innerText = ''
+      screen.innerText = nums[0]
     }
-  } else if (buttonType === 'equals') {
+  } else if (input === 'EQUALS') {
     if (nums.length > 0) {
       nums.push(Number(screen.innerText))
       result = result || operate(operationType, nums)
@@ -94,14 +71,85 @@ const processInput = e => {
     }
   } else {
     if (screen.innerText === zeroDivisionErrorMsg) screen.innerText = ''
-    screen.innerText += buttonContent
+    if (operatorPressed) {
+      operatorPressed = !operatorPressed
+      screen.innerText = ''
+    }
+    screen.innerText += input
   }
 }
 
+const inputMap = input => {
+  let output
+
+  switch (input) {
+    case '+':
+    case 'Add':
+      output = '+'
+      break
+    case '-':
+    case 'Subtract':
+      output = '-'
+      break
+    case '*':
+    case 'Multiply':
+      output = '*'
+      break
+    case '/':
+    case 'Divide':
+      output = '/'
+      break
+    case 'Backspace':
+      output = 'DEL'
+      break
+    case '=':
+    case 'Enter':
+      output = 'EQUALS'
+      break
+    case '.':
+    case 'Decimal':
+      output = '.'
+      break
+    case 'AC':
+      output = 'AC'
+      break
+    default:
+      output = input
+  }
+
+  return output
+}
+
+const getKeyBoardInput = e => {
+  const key = e.code
+  const isAllowedKey = key =>
+    key.includes('Numpad') || key.includes('Digit') || key === 'Backspace'
+
+  if (key && key !== 'NumLock' && isAllowedKey(key)) {
+    let output
+
+    const isNum = key.match(/[0-9]/g)
+    let keyboardInput = isNum
+      ? Number(isNum[0])
+      : key === 'Backspace'
+      ? key
+      : key.split('Numpad')[1]
+
+    output = inputMap(keyboardInput)
+    processInput(output)
+  }
+}
+
+const getButtonInput = e => {
+  const buttonInput = e.target.innerText
+  let output
+
+  output = inputMap(buttonInput)
+  processInput(output)
+}
+
 // Listen for Button input
-allAbuttonsArr.forEach(button =>
-  button.addEventListener('click', e => processInput(e))
-)
+allAbuttonsArr.forEach(button => button.addEventListener('click', e => getButtonInput(e)))
 
 // Listen for Keyinput
 document.addEventListener('keydown', e => getKeyBoardInput(e))
